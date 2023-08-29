6f89068a062bd4d8a4972963d3ad9af84034904c
Catalin <catalinbuza31@gmail.com>
2023-08-29 21:04:38 +0300

    added category controller

---
+++ BlazorProject/Server/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using BlazorProject.Server.Services.CategoryService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorProject.Server.Controllers
+{
+     [Route("api/[controller]")]
+     [ApiController]
+     public class CategoryController : ControllerBase
+     {
+          private readonly ICategoryService _categoryService;
+
+          public CategoryController(ICategoryService categoryService)
+          {
+               _categoryService = categoryService;
+          }
+
+          // GET: api/Product
+          [HttpGet]
+          public async Task<ActionResult<ServiceResponse<List<Category>>>> GetCategorys()
+          {
+               var result = await _categoryService.GetCategorysAsync();
+               return Ok(result);
+          }
+
+          // GET: api/Product/5
+          [HttpGet("{id}")]
+          public async Task<ActionResult<Category>> GetCategory(int id)
+          {
+               var result = await _categoryService.GetCategoryAsync(id);
+               return Ok(result);
+          }
+
+          [HttpPost]
+          public async Task<ActionResult<Category>> AddCategory(Category category)
+          {
+               var result = await _categoryService.AddCategoryAsync(category);
+               return Ok(result);
+          }
+
+          [HttpDelete("{id}")]
+          public async Task<ActionResult<Category>> DeleteCategory(int id)
+          {
+               var result = await _categoryService.DeleteCategoryAsync(id);
+               return Ok(result);
+          }
+
+          [HttpPut("{id}")]
+          public async Task<ActionResult> UpdateCategory(int id, Category category)
+          {
+               var result = await _categoryService.UpdateCategoryAsync(id, category);
+               return Ok(result);
+          }
+     }
+}
