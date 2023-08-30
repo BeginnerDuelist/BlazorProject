84108987f2b8c0bf42dd3c108a8a1ef5398b9b0f
Catalin <catalinbuza31@gmail.com>
2023-08-30 11:56:19 +0300

    added 1 space

--- BlazorProject/Shared/ServiceResponse.cs
+++ BlazorProject/Shared/ServiceResponse.cs
@@ -8,7 +8,7 @@ namespace BlazorProject.Shared
 {
      public class ServiceResponse<T>
      {
-          public T Data { get; set; }
+          public T  Data { get; set; }
           public bool Success { get; set; } = true;
           public string Message { get; set; }=string.Empty;
      }
