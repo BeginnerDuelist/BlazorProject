0d207dbd81af13c539840586c430412ea85e405c
BeginnerDuelist <catalinbuza31@gmail.com>
2023-09-11 10:25:22 +0300

    Restricted lenght for Product Title

--- BlazorProject/Shared/Product.cs
+++ BlazorProject/Shared/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BlazorProject.Shared
      public class Product
      {
           public int Id { get; set; }
+          [StringLength(70, MinimumLength = 25)]
           public string Title { get; set; }=string.Empty;
           public string Description { get; set; } = string.Empty;
           public string ImageUrl { get; set; } = string.Empty;
