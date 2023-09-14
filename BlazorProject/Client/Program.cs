78fa5cbf70f2e5b2634839f425b82f7aaa270db5
BeginnerDuelist <catalinbuza31@gmail.com>
2023-09-14 22:18:28 +0300

    added localstorage cart

--- BlazorProject/Client/Program.cs
+++ BlazorProject/Client/Program.cs
@@ -1,4 +1,6 @@
+using Blazored.LocalStorage;
 using BlazorProject.Client;
+using BlazorProject.Client.Services.CartService;
 using BlazorProject.Client.Services.CategoryService;
 using BlazorProject.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
@@ -11,4 +13,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddBlazoredLocalStorage();
 await builder.Build().RunAsync();
---
+++ BlazorProject/Shared/MyCartModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Shared
+{
+     public class MyCartModel
+     {
+          public int ProductId { get; set; }
+          public int UserQuantity { get; set; }
+     }
+}
