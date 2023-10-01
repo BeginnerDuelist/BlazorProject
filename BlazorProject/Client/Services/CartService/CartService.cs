a9f7fe9c54df5f70eedcedbb971e67092b3c258e
BeginnerDuelist <catalinbuza31@gmail.com>
2023-10-01 19:59:52 +0300

    changes

--- BlazorProject/Client/Services/CartService/CartService.cs
+++ BlazorProject/Client/Services/CartService/CartService.cs
@@ -1,7 +1,11 @@
 ﻿using Blazored.LocalStorage;
+using BlazorProject.Client.Services.AuthService;
 using BlazorProject.Client.Services.ProductService;
 using BlazorProject.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.JSInterop;
+using System;
 
 namespace BlazorProject.Client.Services.CartService
 {
@@ -10,81 +14,105 @@ namespace BlazorProject.Client.Services.CartService
           private readonly ILocalStorageService localStorageService;
           private readonly IJSRuntime jSRuntime;
           private readonly IProductService productService;
+          private readonly IAuthService authService;
+          private readonly NavigationManager navigationManager;
 
-          public CartService(ILocalStorageService localStorageService, IJSRuntime jSRuntime, IProductService productService)
-          {
+        public CartService(ILocalStorageService localStorageService, IJSRuntime jSRuntime,
+        IProductService productService, IAuthService authService, NavigationManager navigationManager)
+        {
                this.localStorageService = localStorageService;
                this.jSRuntime = jSRuntime;
                this.productService = productService;
-          }
+               this.authService = authService;
+               this.navigationManager = navigationManager;
+        }
 
           public int Count { get; set; }
 
           public event Action? OnChange;
 
           public async Task AddToCart(MyCartModel myCartModel)
-          {
-               var MyCart = await localStorageService.GetItemAsync<List<MyCartModel>>("MyCart");
-               if (MyCart == null)
+          {    
+               if(await authService.IsUserAuthenticated())
                {
-                    MyCart = new List<MyCartModel>();
-               }
-
-               if (myCartModel != null)
-               {
-                    var item = MyCart.FirstOrDefault(p => p.ProductId == myCartModel.ProductId);
-                    if (item != null)
+                    var MyCart = await localStorageService.GetItemAsync<List<MyCartModel>>("MyCart");
+                    if (MyCart == null)
                     {
-                         MyCart.Remove(item);
-                         MyCart.Add(myCartModel);
-                         await localStorageService.SetItemAsync("MyCart", MyCart);
-                         await jSRuntime.InvokeVoidAsync("alert", "Product updated done!");
+                        MyCart = new List<MyCartModel>();
+                    }
+
+                    if (myCartModel != null)
+                    {
+                        var item = MyCart.FirstOrDefault(p => p.ProductId == myCartModel.ProductId);
+                        if (item != null)
+                        {
+                            MyCart.Remove(item);
+                            MyCart.Add(myCartModel);
+                            await localStorageService.SetItemAsync("MyCart", MyCart);
+                            await jSRuntime.InvokeVoidAsync("alert", "Product updated done!");
+                        }
+                        else
+                        {
+                            MyCart.Add(myCartModel);
+                            await localStorageService.SetItemAsync("MyCart", MyCart);
+                            await jSRuntime.InvokeVoidAsync("alert", "Product added to cart!");
+                        }
                     }
                     else
                     {
-                         MyCart.Add(myCartModel);
-                         await localStorageService.SetItemAsync("MyCart", MyCart);
-                         await jSRuntime.InvokeVoidAsync("alert", "Product added to cart!");
+                        await jSRuntime.InvokeVoidAsync("alert", "Product cart cannot be empty");
                     }
+
+                    Count = (await localStorageService.GetItemAsync<List<MyCartModel>>("MyCart")).Count();
+                    OnChange?.Invoke();
                }
                else
                {
-                    await jSRuntime.InvokeVoidAsync("alert", "Product cart cannot be empty");
+                    await jSRuntime.InvokeVoidAsync("alert","You need to login first");
                }
-
-               Count = (await localStorageService.GetItemAsync<List<MyCartModel>>("MyCart")).Count();
-               OnChange?.Invoke();
+              
 
           }
 
           public async Task<List<CartModel>> GetCart()
           {
-               var CartModel = new List<CartModel>();
-               var MyCart = await localStorageService.GetItemAsync<List<MyCartModel>>("MyCart");
-               if (MyCart != null)
-               {
-                    foreach (var item in MyCart)
+                var CartModel = new List<CartModel>();
+                if (await authService.IsUserAuthenticated())
+                {
+                    
+                    var MyCart = await localStorageService.GetItemAsync<List<MyCartModel>>("MyCart");
+                    if (MyCart != null)
                     {
-                         var product = await productService.GetProduct(item.ProductId);
-                         if (product != null)
-                         {
-                              var model = new CartModel()
-                              {
-                                   ProductId = item.ProductId,
-                                   Quantity = item.UserQuantity,
-                                   Name = product.Data!.Title,
-                                   Image = product.Data.ImageUrl,
-                                   Price = product.Data.NewPrice != 0 && product.Data.NewPrice < product.Data.Price ?
-                                  product.Data.NewPrice : product.Data.Price,
-                                   SubTotal = (product.Data.NewPrice != 0 && product.Data.NewPrice < product.Data.Price ?
-                                  product.Data.NewPrice : product.Data.Price) * item.UserQuantity
-                              };
-                              CartModel.Add(model);
-                         }
+                        foreach (var item in MyCart)
+                        {
+                            var product = await productService.GetProduct(item.ProductId);
+                            if (product != null)
+                            {
+                                var model = new CartModel()
+                                {
+                                    ProductId = item.ProductId,
+                                    Quantity = item.UserQuantity,
+                                    Name = product.Data!.Title,
+                                    Image = product.Data.ImageUrl,
+                                    Price = product.Data.NewPrice != 0 && product.Data.NewPrice < product.Data.Price ?
+                                    product.Data.NewPrice : product.Data.Price,
+                                    SubTotal = (product.Data.NewPrice != 0 && product.Data.NewPrice < product.Data.Price ?
+                                    product.Data.NewPrice : product.Data.Price) * item.UserQuantity
+                                };
+                                CartModel.Add(model);
+                            }
 
+                        }
                     }
-               }
-               return CartModel;
+                    
+                }
+                else
+                {
+                    await jSRuntime.InvokeVoidAsync("alert", "You need to be logged to see your cart");
+                    navigationManager.NavigateTo("/");
+                }
+                return CartModel;
+
           }
 
           public async Task RemoveCartItem(int productId)
@@ -100,6 +128,16 @@ namespace BlazorProject.Client.Services.CartService
                     }
                }
           }
-     }
+          public async Task RemoveAllCartItems()
+          {
+            var MyCart = await localStorageService.GetItemAsync<List<MyCartModel>>("MyCart");
+            // Remove all items from the cart in local storage
+            await localStorageService.RemoveItemAsync("MyCart");
+
+            // Update the count and trigger the OnChange event
+            Count = 0;
+            OnChange?.Invoke();
+          }
+    }
 }
 
