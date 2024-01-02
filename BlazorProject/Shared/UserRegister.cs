086105c5fb920b58a7d2e4494e5a38f81d7c2708
BeginnerDuelist <catalinbuza31@gmail.com>
2024-01-02 13:59:25 +0200

    Added regex for password

--- BlazorProject/Shared/UserRegister.cs
+++ BlazorProject/Shared/UserRegister.cs
@@ -11,7 +11,10 @@ namespace BlazorProject.Shared
     {
         [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
-        [Required, StringLength(100, MinimumLength = 6)]
+        [Required(ErrorMessage = "Parola este obligatorie.")]
+        [StringLength(30, MinimumLength = 8, ErrorMessage = "Parola trebuie să aibă între 8 și 30 de caractere.")]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+        ErrorMessage = "Parola trebuie să conțină cel puțin o literă mică, o literă mare, o cifră și un caracter special.")]
         public string Password { get; set; } = string.Empty;
         [Compare("Password", ErrorMessage = "The passwords do not match.")]
         public string ConfirmPassword { get; set; } = string.Empty;
